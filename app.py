e8ee1810fdddcced46306951ce53274b20e905ac
Iscaraca <isaac9503@gmail.com>
2024-03-01 11:16:49 +0800

    Add port randomisation and redirect

--- app.py
+++ app.py
@@ -1,37 +1,44 @@
-from flask import Flask, render_template, request, jsonify, Response
+from flask import Flask, render_template, redirect, url_for, session
 import threading
 import subprocess
-import os
+import random
 
 app = Flask(__name__)
+app.secret_key = 'change_me'
 sem = threading.Semaphore()
 
-port = 4000
+ports = []
 
 def start_instance(port):
     subprocess.run(["sh", "./run-compose.sh", f"instance{port}", port], cwd="/app")
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
 @app.route('/generate', methods=['GET'])
 def generate():
     global sem
-
     sem.acquire()
-    global port
+
+    global ports
+    port = random.randint(1000, 10000)
+    while port in ports:
+        port = random.randint(1000, 10000)
+    
+    ports.append(port)
+    port = str(port)
+
     thread = threading.Thread(target=start_instance, args=(port))
     thread.start()
-    port += 1
+
     sem.release()
 
-    return render_template('index.html', port=port)
+    session['port'] = port
+    return redirect(url_for('instancer'))
 
     
 @app.route('/', methods=['GET'])
 def instancer():
-    return render_template('index.html')
+    port = session.get('port')
+    session.clear()
+    return render_template('index.html', port=port)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
